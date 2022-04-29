c868d48cf77ae785bbd45990a4e17528eae264f5
Michael Delcambre <delcambrem@gmail.com>
2022-04-28 22:10:22 -0500

    progress with filter 2

--- src/Components/Input1.jsx
+++ src/Components/Input1.jsx
@@ -21,13 +21,31 @@ const Input1 = () => {
   // console.log(alphabet);
   // const aaa = inputtest.split("");
 
-  const filtertest = inputtest
+  const englishOrdinal = inputtest
     ? alphabet
         .filter((x) => x.letter === inputtest)
         .map((filteredLetter) => filteredLetter.values[0].english_ordinal)
     : null;
 
-  console.log(filtertest);
+  const fullReduction = inputtest
+    ? alphabet
+        .filter((x) => x.letter === inputtest)
+        .map((filteredLetter) => filteredLetter.values[0].full_reduction)
+    : null;
+  const reverseOrdinal = inputtest
+    ? alphabet
+        .filter((x) => x.letter === inputtest)
+        .map((filteredLetter) => filteredLetter.values[0].reverse_ordinal)
+    : null;
+  const reverseFullReduction = inputtest
+    ? alphabet
+        .filter((x) => x.letter === inputtest)
+        .map(
+          (filteredLetter) => filteredLetter.values[0].reverse_full_reduction
+        )
+    : null;
+
+  // console.log(englishOrdinal);
 
   return (
     <div>
@@ -44,7 +62,13 @@ const Input1 = () => {
               <li key={v4()}>{filteredLetter.values.english_ordinal}</li>
             ))
         : null} */}
-      {filtertest}
+      English Ordinal: {englishOrdinal}
+      <br></br>
+      Full Reduction: {fullReduction}
+      <br></br>
+      Reverse Ordinal: {reverseOrdinal}
+      <br></br>
+      Reverse Full Ordinal: {reverseFullReduction}
     </div>
   );
 };
