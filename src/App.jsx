e1c864dc23aa444b53f1a272c831fffc7407cbfb
Michael Delcambre <delcambrem@gmail.com>
2022-04-28 20:57:44 -0500

    z

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Input1 from './Components/Input1'
+// import {useAtom} from 'jotai'
+// import {alphabetAtom} from './State.jsx'
+
 
 function App() {
+
+  // const [x, setx] = useAtom(alphabetAtom)
+
+  // console.log(x)
 
   return (
     <div className="App">
