5e84bb79aafcff75cd5949de027f46e8f2f4d5c5
Michael Delcambre <delcambrem@gmail.com>
2022-04-28 20:56:16 -0500

    stuck at filtering

--- src/Components/Input1.jsx
+++ src/Components/Input1.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
+import { useAtom } from "jotai";
+import { alphabetAtom } from "../State.jsx";
+import { v4 } from "uuid";
 
 const Input1 = () => {
-  const [alphabet, setAlphabet] = useState(null);
+  const [alphabet, setAlphabet] = useAtom(alphabetAtom);
   const [inputtest, setinputtest] = useState("");
 
   const fetchAlphabet = async () => {
     const data = await fetch("http://localhost:3000/alphabets");
     const json = await data.json();
-    // console.log(json)
     setAlphabet(json);
   };
 
@@ -16,11 +18,9 @@ const Input1 = () => {
     fetchAlphabet().catch(console.error);
   }, []);
 
-  console.log(alphabet)
-
-  const aaa = inputtest.split("");
-  //   console.log(aaa)
-  //   console.log(inputtest)
+  // console.log(alphabet);
+  // const aaa = inputtest.split("");
+  console.log();
 
   return (
     <div>
@@ -30,7 +30,13 @@ const Input1 = () => {
         onChange={(e) => setinputtest(e.target.value)}
       ></input>
       <br></br>
-      {aaa}
+      {inputtest
+        ? alphabet
+            .filter((x) => x.letter === inputtest)
+            .map((filteredLetter) => (
+              <li key={v4()}>{filteredLetter.values.english_ordinal}</li>
+            ))
+        : null}
     </div>
   );
 };
